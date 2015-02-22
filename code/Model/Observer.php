de74ad390813e97fcec66b2595531d0118cb92b6
Guillaume Moigneu Niels <n@nls.io>
2015-02-22 03:06:21 +0100

    Remove dead code

--- code/Model/Observer.php
+++ code/Model/Observer.php
@@ -8,6 +8,11 @@ class Nls_Crowd_Model_Observer
     const CONFIG_PATH_CROWD_APP_NAME = 'nls_crowd/config/app_name';
     const CONFIG_PATH_CROWD_APP_PASSWORD = 'nls_crowd/config/app_credential';
 
+    /**
+     * Catch admin controller predispatch & check for login data
+     * 
+     * @param $event
+     */
     public function adminhtmlControllerActionPredispatchStart($event)
     {
         $this->request = Mage::app()->getRequest();
@@ -27,48 +32,5 @@ class Nls_Crowd_Model_Observer
                 $crowd->authenticate($username, $password);
             }
         }
-    }
-    protected function userAuthenticated($username, $role)
-    {
-        // TODO core/session is initialized in
-        // app/code/core/Mage/Core/Controller/Varien/Action.php:495
-        // this are 30 lines after the event we are observing (adminhtmlControllerActionPredispatchStart)
-        Mage::getSingleton(
-            'core/session',
-            array('name' => Mage_Adminhtml_Controller_Action::SESSION_NAMESPACE)
-        )->start();
-        /* @var $session Mage_Admin_Model_Session */
-        $session = Mage::getSingleton('admin/session');
-        /* @var $user Mage_Admin_Model_User */
-        $user = Mage::getModel('admin/user');
-        $user->loadByUsername($username);
-        if (!$user->getId()) {
-            $name = uniqid();
-            $user->setUsername($username);
-            $user->setFirstname($name);
-            $user->setLastname($name);
-            $user->setPassword('test');
-            $user->setEmail($name . '@domain.invalid');
-            $user->setIsActive(true);
-            $role = Mage::getModel('admin/role')->load($role, 'role_name');
-            if (!$role->getId()) {
-                Mage::throwException('Role not found.');
-            }
-            $user->save();
-            $user->setRoleIds(array($role->getId()))
-                ->setRoleUserId($user->getUserId())
-                ->saveRelations();
-        }
-        // TODO copied from app/code/core/Mage/Admin/Model/User.php:339
-        // any better idea?
-        if ($user->getIsActive() != '1') {
-            Mage::throwException(Mage::helper('adminhtml')->__('This account is inactive.'));
-        }
-        if (!$user->hasAssigned2Role($user->getId())) {
-            Mage::throwException(Mage::helper('adminhtml')->__('Access denied.'));
-        }
-        $session->setIsFirstPageAfterLogin(true);
-        $session->setUser($user);
-        $session->setAcl(Mage::getResourceModel('admin/acl')->loadAcl());
     }
 }
